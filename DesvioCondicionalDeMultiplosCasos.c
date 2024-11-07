9120eab3c444aa2f92926ad79b51d0d13dedfd8e
JuaoSousa <joaosilvx42@gmail.com>
2024-11-07 11:06:58 -0300

    Desvio Condicional de Multiplos Casos

---
+++ DesvioCondicionalDeMultiplosCasos.c
@@ -0,0 +1,24 @@
+/*
+Desvio Condicional de Multiplos casos
+
+switch (codigo)
+{
+case1:
+comando1;
+break;
+
+case2:
+comando2;
+break;
+
+case3:
+comando3;
+break;
+
+default:
+comando4;
+break;
+
+}
+
+*/
