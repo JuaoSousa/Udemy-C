323e8d5688faf95b085f91997802898c438a8402
JoaoSilva <joaosilvx42@gmail.com>
2024-11-22 14:50:48 -0300

    Create OperadoresLogicos.c

---
+++ output/OperadoresLogicos.c
@@ -0,0 +1,12 @@
+/*
+Operador Logico .E.
+
+Operador Lógico
+      E
+
+Teste 1 | E | Teste 2 |Resultado final
+  V     | E |   V     |       V
+  V     | E |   F     |       F
+  F     | E |   V     |       F
+  F     | E |   F     |       F
+  */
